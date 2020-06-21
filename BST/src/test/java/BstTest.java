91ed3604a80e791a508db14c807688aa8bb6b288
Francisco Thiago de Macedo Medeiros <fthiagomedeiros@gmail.com>
2020-06-21 03:02:33 -0300

    BST improvement tests

--- BST/src/test/java/BstTest.java
+++ BST/src/test/java/BstTest.java
@@ -9,21 +9,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BstTest {
 
     @Test
-    @DisplayName("Three items BST")
+    @DisplayName("searching 1 in 3 items array")
     void checkTreeThreeElementsStack() throws EmptyArrayException, NotFoundKey {
         int index = Bst.search(new int[] {1,2,3}, 1);
-        assertEquals(0, index, "Empty should be true");
+        assertEquals(0, index, String.format("Key %s should be in index %s", 1, 0));
     }
 
     @Test
-    @DisplayName("Ten items BST")
+    @DisplayName("searching 10 in 10 items array")
     void checkTenElementsStack() throws EmptyArrayException, NotFoundKey {
         int index = Bst.search(new int[] {1,2,3,4,5,6,7,8,9,10}, 10);
-        assertEquals(9, index, "Empty should be true");
+        assertEquals(9, index, String.format("Key %s should be in index %s", 10, 9));
     }
 
     @Test
-    @DisplayName("Not Found Item")
+    @DisplayName("searching 1 in 1 item array")
+    void checkOneElementArrayOfOne() throws EmptyArrayException, NotFoundKey {
+        int index = Bst.search(new int[] {1}, 1);
+        assertEquals(0, index, String.format("Key %s should be in index %s", 10, 9));
+    }
+
+    @Test
+    @DisplayName("Not Found Key Item")
     void checkOneElement() throws NotFoundKey {
         Assertions.assertThrows(NotFoundKey.class, () -> {
             Bst.search(new int[] {1}, 12);
@@ -31,7 +38,7 @@ public class BstTest {
     }
 
     @Test
-    @DisplayName("EmptyArrayException Thrown")
+    @DisplayName("Array for searching is empty")
     void checkEmptyArray() {
         Assertions.assertThrows(EmptyArrayException.class, () -> {
             Bst.search(new int[] {}, 1);

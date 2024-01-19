b9b379b819014b6238f565f670121f308deddeb3
Thiago Medeiros <fthiagomedeiros@gmail.com>
2024-01-19 10:31:35 +0000

    Improving algorithm to calculate bestprice

--- besttimetobuy/src/main/java/org/example/Solution.java
+++ besttimetobuy/src/main/java/org/example/Solution.java
@@ -1,42 +1,40 @@
 package org.example;
 
 import java.util.Arrays;
-import java.util.OptionalInt;
 
 class Solution {
 
-    public int maxProfit(int[] prices) {
-        int maxProfit = 0;
+  public int maxProfit(int[] prices) {
+    int numberOfDays = prices.length;
 
-        int numberOfdays = prices.length;
-
-        for (int i = 0; i < numberOfdays; i++) {
-
-            OptionalInt maxValueFromIndex = Arrays
-                .stream(Arrays.copyOfRange(prices, i+1, numberOfdays))
-                .max();
-
-            int maxValue = maxValueFromIndex.isPresent() ?
-                maxValueFromIndex.getAsInt() : -1;
-            int dayValue = prices[i];
-
-            if (dayValue > maxValue) {
-            } else {
-                int profit = maxValue - dayValue;
-                if (profit > maxProfit) {
-                    maxProfit = profit;
-                }
-            }
-        }
-
-        return maxProfit;
+    if (numberOfDays <= 1) {
+      return 0;
     }
 
-    private int[] copyOfRangeInt(int[] original, int from, int to) {
-        int newLength = to - from;
-        int[] copy = new int[newLength];
-        System.arraycopy(original, from, copy, 0,
-                         Math.min(original.length - from, newLength));
-        return copy;
+    int[] maxProfitArray = new int[numberOfDays];
+    int minPrice = prices[0];
+
+    for (int i = 1; i < numberOfDays; i++) {
+      int currentPrice = prices[i];
+
+      // Updates the minimum
+      minPrice = Math.min(minPrice, currentPrice);
+
+      // Calculates the profit to sell on day + 1
+      int potentialProfit = currentPrice - minPrice;
+
+      // Updates the max profit until the day
+      maxProfitArray[i] = Math.max(maxProfitArray[i - 1], potentialProfit);
     }
+
+    return maxProfitArray[numberOfDays - 1];
+  }
+
+  private int[] copyOfRangeInt(int[] original, int from, int to) {
+    int newLength = to - from;
+    int[] copy = new int[newLength];
+    System.arraycopy(original, from, copy, 0,
+        Math.min(original.length - from, newLength));
+    return copy;
+  }
 }
--- besttimetobuy/src/test/java/org/example/SolutionTest.java
+++ besttimetobuy/src/test/java/org/example/SolutionTest.java
@@ -17,20 +17,29 @@ public class SolutionTest {
 
   @Test
   public void shouldCalculateNoBestTimeToBuyAndSell() {
-    int[] prices = {7,6,4,3,1};
+    int[] prices = {7, 6, 4, 3, 1};
 
     Solution s = new Solution();
     int result = s.maxProfit(prices);
     assertEquals(result, 0);
   }
 
-    @Test
+  @Test
   public void shouldCalculateBestTimeToBuyAndSell2() {
-    int[] prices = {1,2,3,4,5,6,7,8,9,10,1,20};
+    int[] prices = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 20};
 
     Solution s = new Solution();
     int result = s.maxProfit(prices);
     assertEquals(result, 19);
+  }
+
+  @Test
+  public void shouldCalculateBestTimeToBuyAndSell3() {
+    int[] prices = {885,1,290,892,316,799,642,292,107,889,497,330,231,738,420,37,807,241,417,964,736,834,618,265,389,763,378,830,840,131,495,271,707,502,148,561,644,484,868,53,936,186,35,219,8000,84,455,971,922,862,434,553,948,857,491,622,162,934,66,486,569,690,596,506,452,635,690};
+
+    Solution s = new Solution();
+    int result = s.maxProfit(prices);
+    assertEquals(result, 7999);
   }
 
 }

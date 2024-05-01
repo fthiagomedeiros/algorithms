244e5794593c8f2267944753f9728c8ec5eb856f
Francisco Thiago <fthiagomedeiros@gmail.com>
2024-05-01 18:58:12 +0100

    Exercise 01

--- my-custom-bst/src/main/java/FizzBuzz.java
+++ my-custom-bst/src/main/java/FizzBuzz.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 public class FizzBuzz {
 
@@ -20,13 +28,6 @@ public class FizzBuzz {
             i++;
         }
     }
-
-    public static void main(String[] args) {
-//        fizzBuzz(15);
-        List<String> result = level1Fizz(List.of(1, 2, 3, 4, 5, 6));
-        System.out.println(result);
-    }
-
 
     /**
      * Return a list where any number evenly divisible by 3 is replaced with
@@ -78,5 +79,121 @@ public class FizzBuzz {
 
         return result;
     }
+
+
+    /**
+     * Reverse the given input and return the result
+     */
+    public static String reverseString(String inputString) {
+        // Write your code here
+
+        StringBuilder temp = new StringBuilder();
+
+        int textSizeIndex = inputString.length() - 1;
+        while (textSizeIndex >= 0) {
+            temp.append(inputString.charAt(textSizeIndex));
+            textSizeIndex--;
+        }
+        return temp.toString();
+    }
+
+    public static void main(String[] args) {
+//        fizzBuzz(15);
+//        List<String> result = level1Fizz(List.of(1, 2, 3, 4, 5, 6));
+//        System.out.println(result);
+//            reverseString("Thiago");
+
+        //int result = level1WordCount("The quick brown fox jumps over the lazy dog.");
+//        List<String> result2 = level2WordSorting("The quick brown fox jumps over the lazy dog, ant, AN");
+
+        String sentence = "this is a test sentence is is test";
+        List<String> sortedWords = level3SortByOccurrences(sentence);
+        System.out.println(sortedWords);
+    }
+
+
+
+    /**
+     * Take a sentence as input and count the number of words and return it
+     * as an int.
+     */
+    public static int level1WordCount(String sentence) {
+        String[] result = sentence.split(" ");
+        return result.length;
+    }
+
+
+    /**
+     * Take a sentence as input and return a list of the words in the
+     * sentence, sorted alphabetically in lowercase and without punctuation
+     * (,.!?). If there are multiple occurrences of the same word, include it
+     * multiple times.
+     */
+    public static List<String> level2WordSorting(String sentence) {
+        List<String> result = new ArrayList<>();
+        String[] inputSentence = sentence.split(" ");
+
+        for (int i = 0; i < inputSentence.length; i++) {
+            String current = inputSentence[i];
+            current = current.replaceAll("\\p{Punct}", "");
+            result.add(current.toLowerCase());
+        }
+
+        Collections.sort(result);
+        return result;
+    }
+
+
+
+    /**
+     * Take a sentence as input and sort the words in it by the number of
+     * times they appear in the sentence. In the case of a tie, the words
+     * should be sorted alphabetically.
+     */
+    public static List<String> level3SortByOccurrences(String sentence) {
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        String[] inputSentence = sentence.split(" ");
+
+        for (int i = 0; i < inputSentence.length; i++) {
+            String current = inputSentence[i].replaceAll("\\p{Punct}", "").toLowerCase();
+            wordCount.put(current, wordCount.getOrDefault(current, 0) + 1);
+        }
+
+        List<String> sortedWords = new ArrayList<>(wordCount.keySet());
+
+        sortedWords.sort((a, b) -> {
+            int freqComparison = wordCount.get(b).compareTo(wordCount.get(a));
+            if (freqComparison != 0) {
+                return freqComparison; // Sort by frequency if frequencies are different
+            } else {
+                return a.compareTo(b); // Sort alphabetically if frequencies are the same
+            }
+        });
+
+        return sortedWords;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    static class Word implements Comparable<Word> {
+        private int occurrence;
+        private String word;
+
+        @Override
+        public int compareTo(Word obj) {
+            if (!this.toString().equalsIgnoreCase(obj.toString())) {
+                return Integer.compare(this.occurrence, obj.occurrence);
+            }
+            return 0;
+        }
+
+        @Override
+        public String toString() {
+            return word;
+        }
+    }
+
 
 }

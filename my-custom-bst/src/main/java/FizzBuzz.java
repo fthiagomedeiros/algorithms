78e4423c51221b760ce8c22d06f25035a1d73e65
Francisco Thiago <fthiagomedeiros@gmail.com>
2024-05-01 17:22:44 +0100

    Exercise 01

---
+++ my-custom-bst/src/main/java/FizzBuzz.java
@@ -0,0 +1,25 @@
+public class FizzBuzz {
+
+    public static void fizzBuzz(int n) {
+
+        int i = 1;
+        while (n >= i) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        fizzBuzz(15);
+    }
+
+}

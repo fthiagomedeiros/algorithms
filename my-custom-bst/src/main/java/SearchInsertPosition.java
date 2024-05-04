1be33b510eb02678690e327b3f6f7e90e8a6400c
Francisco Thiago <fthiagomedeiros@gmail.com>
2024-05-04 13:35:30 +0100

    find-first-and-last-position-of-element-in-sorted-array/

--- my-custom-bst/src/main/java/SearchInsertPosition.java
+++ my-custom-bst/src/main/java/SearchInsertPosition.java
@@ -1,30 +1,74 @@
 public class SearchInsertPosition {
 
 
-        public int searchInsert(int[] nums, int target) {
+    public int searchInsert(int[] nums, int target) {
 
-            int low = 0;
-            int high = nums.length - 1;
+        int low = 0;
+        int high = nums.length - 1;
 
-            while (low <= high) {
-                // Calculate the midpoint in a way that avoids overflow
-                int mid = low + (high - low) / 2;
+        while (low <= high) {
+            // Calculate the midpoint in a way that avoids overflow
+            int mid = low + (high - low) / 2;
 
-                if (nums[mid] == target) {
-                    // If the target is found, return the index
-                    return mid;
-                } else if (nums[mid] > target) {
-                    // Search the left half
-                    high = mid - 1;
+            if (nums[mid] == target) {
+                // If the target is found, return the index
+                return mid;
+            } else if (nums[mid] > target) {
+                // Search the left half
+                high = mid - 1;
+            } else {
+                // Search the right half
+                low = mid + 1;
+            }
+        }
+
+        // If the target is not found, return the index where it could be inserted
+        return low;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+
+            if (nums.length == 1) {
+                int value = nums[0];
+                if (value == target) {
+                    return new int[]{0, 0};
                 } else {
-                    // Search the right half
-                    low = mid + 1;
+                    return new int[]{-1, -1};
                 }
             }
 
-            // If the target is not found, return the index where it could be inserted
-            return low;
+            // Calculate the midpoint in a way that avoids overflow
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                // If the target is found, return the index
+
+                int previous = mid - 1;
+                int next = mid + 1;
+
+                if (previous >= 0 && nums[previous] == target) {
+                    return new int[]{previous, mid};
+                } else if (next <= nums.length - 1 && nums[next] == target) {
+                    return new int[]{mid, next};
+                }
+
+                return new int[]{mid, mid};
+            } else if (nums[mid] > target) {
+                // Search the left half
+                high = mid - 1;
+            } else {
+                // Search the right half
+                low = mid + 1;
+            }
         }
+
+        // If the target is not found, return the index where it could be inserted
+        return new int[]{-1, -1};
+    }
 
 
 }
--- my-custom-bst/src/test/java/SearchInsertPositionTest.java
+++ my-custom-bst/src/test/java/SearchInsertPositionTest.java
@@ -26,4 +26,37 @@ class SearchInsertPositionTest {
         int result = s.searchInsert(input, target);
         Assertions.assertEquals(2, result);
     }
+
+    @Test
+    void searchInsertRange1() {
+        SearchInsertPosition s = new SearchInsertPosition();
+
+        int[] input = new int[] {2,2};
+        int target = 2;
+
+        int[] result = s.searchRange(input, target);
+        Assertions.assertEquals(new int[] {0,1}, result);
+    }
+
+    @Test
+    void searchInsertRange2() {
+        SearchInsertPosition s = new SearchInsertPosition();
+
+        int[] input = new int[] {1,4};
+        int target = 4;
+
+        int[] result = s.searchRange(input, target);
+        Assertions.assertEquals(new int[] {1,1}, result);
+    }
+
+    @Test
+    void searchInsertRange3() {
+        SearchInsertPosition s = new SearchInsertPosition();
+
+        int[] input = new int[] {1,1,2};
+        int target = 1;
+
+        int[] result = s.searchRange(input, target);
+        Assertions.assertEquals(new int[] {0,1}, result);
+    }
 }

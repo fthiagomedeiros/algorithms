5826a18a3f1a1242378cf03d2dd6654b20e43a4c
Francisco Thiago <fthiagomedeiros@gmail.com>
2024-05-04 23:50:59 +0100

    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/

--- my-custom-bst/src/main/java/SearchInsertPosition.java
+++ my-custom-bst/src/main/java/SearchInsertPosition.java
@@ -27,36 +27,30 @@ public class SearchInsertPosition {
     }
 
     public int[] searchRange(int[] nums, int target) {
+
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+
+        int[] base = null;
         int low = 0;
         int high = nums.length - 1;
+        base = findBase(nums, target);
+
+        if (base != null) {
+            return base;
+        }
+
+        boolean foundValue = false;
 
         while (low <= high) {
-
-            if (nums.length == 1) {
-                int value = nums[0];
-                if (value == target) {
-                    return new int[]{0, 0};
-                } else {
-                    return new int[]{-1, -1};
-                }
-            }
 
             // Calculate the midpoint in a way that avoids overflow
             int mid = low + (high - low) / 2;
 
             if (nums[mid] == target) {
-                // If the target is found, return the index
-
-                int previous = mid - 1;
-                int next = mid + 1;
-
-                if (previous >= 0 && nums[previous] == target) {
-                    return new int[]{previous, mid};
-                } else if (next <= nums.length - 1 && nums[next] == target) {
-                    return new int[]{mid, next};
-                }
-
-                return new int[]{mid, mid};
+                foundValue = true;
+                return findTarget(mid, nums, target);
             } else if (nums[mid] > target) {
                 // Search the left half
                 high = mid - 1;
@@ -66,9 +60,71 @@ public class SearchInsertPosition {
             }
         }
 
+        if (!foundValue) {
+            return new int[]{-1, -1};
+        }
+
         // If the target is not found, return the index where it could be inserted
-        return new int[]{-1, -1};
+        return base;
     }
 
+    private int[] findBase(int[] nums, int target) {
+        if (nums.length == 1) {
+            int value = nums[0];
+            if (value == target) {
+                return new int[]{0, 0};
+            }
+        }
+        return null;
+    }
 
+    private int[] findTarget(int mid, int[] nums, int target) {
+
+        int[] range = new int[2];
+        range[0] = mid;
+        range[1] = mid;
+
+        boolean lowerFound = false;
+        boolean upperFound = false;
+
+        if (mid == 0) {
+            int start = 0;
+            int end = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == target) {
+                    end = i;
+                    range[0] = start;
+                    range[1] = end;
+                }
+            }
+        } else if (mid > 0 && mid < nums.length) {
+            int start = mid;
+            int end = mid;
+
+            while (!lowerFound || !upperFound) {  /**nums[start] == target || nums[end] == target*/
+                if (!lowerFound && nums[start] == target) {
+                    if (start > 0 && nums[start - 1] == target) {
+                        start--;
+                        range[0] = start;
+                    } else {
+                        start--;
+                        lowerFound = true;
+                    }
+                }
+
+                if (!upperFound && end + 1 <= nums.length - 1 && nums[end + 1] == target) {
+                    end++;
+                    range[1] = end;
+                } else {
+                    end++;
+                    upperFound = true;
+                }
+
+            }
+        }
+
+        return range;
+
+    }
 }
--- my-custom-bst/src/test/java/SearchInsertPositionTest.java
+++ my-custom-bst/src/test/java/SearchInsertPositionTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 class SearchInsertPositionTest {
 
     @Test
-    void searchInsert() {
+    void searchCaseTwoItemsInArray() {
         SearchInsertPosition s = new SearchInsertPosition();
 
-        int[] input = new int[] {1,3};
+        int[] input = new int[]{1, 3};
         int target = 2;
 
         int result = s.searchInsert(input, target);
@@ -17,10 +17,10 @@ class SearchInsertPositionTest {
     }
 
     @Test
-    void searchInsert2() {
+    void searchCaseThreeItemsInArray() {
         SearchInsertPosition s = new SearchInsertPosition();
 
-        int[] input = new int[] {1,3,5};
+        int[] input = new int[]{1, 3, 5};
         int target = 4;
 
         int result = s.searchInsert(input, target);
@@ -28,35 +28,78 @@ class SearchInsertPositionTest {
     }
 
     @Test
-    void searchInsertRange1() {
+    void searchCaseTheSameValueInSmallArray() {
         SearchInsertPosition s = new SearchInsertPosition();
 
-        int[] input = new int[] {2,2};
+        int[] input = new int[]{2, 2};
         int target = 2;
 
         int[] result = s.searchRange(input, target);
-        Assertions.assertEquals(new int[] {0,1}, result);
-    }
 
-    @Test
-    void searchInsertRange2() {
-        SearchInsertPosition s = new SearchInsertPosition();
-
-        int[] input = new int[] {1,4};
-        int target = 4;
-
-        int[] result = s.searchRange(input, target);
-        Assertions.assertEquals(new int[] {1,1}, result);
+        Assertions.assertEquals(0, result[0]);
+        Assertions.assertEquals(1, result[1]);
     }
 
     @Test
     void searchInsertRange3() {
         SearchInsertPosition s = new SearchInsertPosition();
 
-        int[] input = new int[] {1,1,2};
+        int[] input = new int[]{1, 1, 2};
         int target = 1;
 
         int[] result = s.searchRange(input, target);
-        Assertions.assertEquals(new int[] {0,1}, result);
+
+        Assertions.assertEquals(0, result[0]);
+        Assertions.assertEquals(1, result[1]);
     }
+
+    @Test
+    void searchInsertRange333() {
+        SearchInsertPosition s = new SearchInsertPosition();
+
+        int[] input = new int[]{3, 3, 3};
+        int target = 3;
+
+        int[] result = s.searchRange(input, target);
+
+        Assertions.assertEquals(0, result[0]);
+        Assertions.assertEquals(2, result[1]);
+    }
+
+    @Test
+    public void shouldValidate() {
+        SearchInsertPosition s = new SearchInsertPosition();
+
+        int[] input = new int[]{1};
+        int target = 1;
+
+        int[] result = s.searchRange(input, target);
+        assertEquals(2, result.length);
+    }
+
+    @Test
+    void searchInsertRange10() {
+        SearchInsertPosition s = new SearchInsertPosition();
+
+        int[] input = new int[]{1, 2, 2, 2, 2, 2, 3};
+        int target = 2;
+
+        int[] result = s.searchRange(input, target);
+
+        Assertions.assertEquals(1, result[0]);
+        Assertions.assertEquals(5, result[1]);
+    }
+
+    @Test
+    void searchInsertRange11() {
+        SearchInsertPosition s = new SearchInsertPosition();
+
+        int[] input = new int[]{5, 7, 7, 8, 8, 10};
+        int target = 6;
+
+        int[] result = s.searchRange(input, target);
+        Assertions.assertEquals(-1, result[0]);
+        Assertions.assertEquals(-1, result[1]);
+    }
+
 }

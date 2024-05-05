bc5a8146f0502b66b91f61748ff16f4a287b40bd
Francisco Thiago <fthiagomedeiros@gmail.com>
2024-05-05 12:06:44 +0100

    305 / 307 testcases passed
    https://leetcode.com/problems/powx-n

--- my-custom-bst/src/main/java/MyPowIterative.java
+++ my-custom-bst/src/main/java/MyPowIterative.java
@@ -24,8 +24,12 @@ public class MyPowIterative {
             n = n * -1;
         }
 
-        if (n == Integer.MIN_VALUE && negative) {
+        if (n == Integer.MIN_VALUE && negative && x <= 1) {
             return 0.0;
+        }
+
+        if (n == Integer.MIN_VALUE && x > 1) {
+            n = Integer.MAX_VALUE;
         }
 
         double finalResult = 1;

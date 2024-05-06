76829eaf7cbfbf46057dee04001bd8525d88cb2c
Francisco Thiago <fthiagomedeiros@gmail.com>
2024-05-06 01:04:02 +0100

    305 / 307 testcases passed
    https://leetcode.com/problems/powx-n

--- my-custom-bst/src/main/java/ValidBstAlgorithm.java
+++ my-custom-bst/src/main/java/ValidBstAlgorithm.java
@@ -2,45 +2,95 @@
 public class ValidBstAlgorithm {
 
     public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, root, 0);
+        return true;
     }
 
-    private boolean isValidBST(TreeNode root, TreeNode mainRoot, int position) {
-        int leftPos = 1;
-        int rightPos = 2;
+//    public boolean isValidBST(TreeNode root) {
+//        return isValidBST(root, root, 0);
+//    }
 
-        TreeNode left = root.left;
-        TreeNode right = root.right;
+//    private boolean isValidBST(TreeNode root, TreeNode mainRoot, int position) {
+//        int leftPos = 1;
+//        int rightPos = 2;
+//
+//        TreeNode left = root.left;
+//        TreeNode right = root.right;
+//
+//        if (left == null && right == null) {
+//            return true;
+//        }
+//
+//        if (left == null) {
+//            if (right.val <= root.val) {
+//                return false;
+//            }
+//            return true;
+//        } else if (right == null) {
+//            if (left.val >= root.val) {
+//                return false;
+//            }
+//            return true;
+//        }
+//
+//        if ((left != null && left.val >= root.val) || (right != null && right.val <= root.val)) {
+//            return false;
+//        }
+//
+//        if (position == leftPos && (left != null && left.val >= mainRoot.val || (right != null && right.val >= mainRoot.val))) {
+//            return false;
+//        } else if (position == rightPos && left.val <= mainRoot.val) {
+//            return false;
+//        }
+//
+//        boolean checkLeft = isValidBST(left, root, leftPos); //left subtree
+//        boolean checkRight = isValidBST(right, root, rightPos); //right subtree
+//        return checkLeft && checkRight;
+//    }
 
-        if (left == null && right == null) {
-            return true;
+    /**
+    class Solution {
+        public boolean isValidBST(TreeNode root) {
+            return isValidBST(root, root, 0);
         }
 
-        if (left == null) {
-            if (right.val <= root.val) {
+        private boolean isValidBST(TreeNode root, TreeNode mainRoot, int position) {
+            int leftPos = 1;
+            int rightPos = 2;
+
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+
+            if (left == null && right == null) {
+                return true;
+            }
+
+            if (left == null) {
+                if (right.val <= root.val) {
+                    return false;
+                }
+                return true;
+            } else if (right == null) {
+                if (left.val >= root.val) {
+                    return false;
+                }
+                return true;
+            }
+
+            if ((left != null && left.val >= root.val) || (right != null && right.val <= root.val)) {
                 return false;
             }
-            return true;
-        } else if (right == null) {
-            if (left.val >= root.val) {
+
+            if (position == leftPos && (left != null && left.val >= mainRoot.val || (right != null && right.val >= mainRoot.val))) {
+                return false;
+            } else if (position == rightPos && left.val <= mainRoot.val) {
                 return false;
             }
-            return true;
-        }
 
-        if ((left != null && left.val >= root.val) || (right != null && right.val <= root.val)) {
-            return false;
+            boolean checkLeft = isValidBST(left, root, leftPos); //left subtree
+            boolean checkRight = isValidBST(right, root, rightPos); //right subtree
+            return checkLeft && checkRight;
         }
-
-        if (position == leftPos && (left != null && left.val >= mainRoot.val || (right != null && right.val >= mainRoot.val))) {
-            return false;
-        } else if (position == rightPos && left.val <= mainRoot.val) {
-            return false;
-        }
-
-        boolean checkLeft = isValidBST(left, root, leftPos); //left subtree
-        boolean checkRight = isValidBST(right, root, rightPos); //right subtree
-        return checkLeft && checkRight;
     }
+     */
 
 }
---
+++ my-custom-bst/src/test/java/DFSAlgorithmTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DFSAlgorithmTest {
+
+    @Test
+    void dfs1() {
+
+    }
+}
